3a03c6214e1c9f256e1577d8b618e0e018035ee9
Shakeb Siddiqui <sms8508@psu.edu>
2020-08-28 23:18:34 +0000

    float

---
+++ main.py
@@ -0,0 +1,4 @@
+varcel = input ("Enter temperature in celsius: ")
+varfer = (float(varcel)/5)*(9)+32
+varcel = float(varcel)
+print(str(varcel)+"° in Celsius is equal to",str(varfer)+"°", "Fahrenheit.")
